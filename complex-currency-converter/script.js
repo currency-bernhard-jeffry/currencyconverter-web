23d1703075f328c17d7f52a1acd25fcffbddc7f3
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-03 15:09:45 +0000

    feat: update

--- complex-currency-converter/script.js
+++ complex-currency-converter/script.js
@@ -2,17 +2,55 @@ const fromCurrency = document.querySelector("#from-currency");
 const toCurrency = document.querySelector("#to-currency");
 const converterContainer = document.querySelector("#converter-container");
 const currencyInput = document.querySelector("#currency-input");
+const fromResult = document.querySelector("#from-result");
+const toResult = document.querySelector("#to-result");
 
+function calculateCurrency() {
+  const rate = rateData.result;
+  const convertedCurrency = parseInt(currencyInput.value) * rate;
+  console.log(convertedCurrency);
+}
+
+let rateData = [];
+let currencyDescription = [];
+async function getRateAndCurrencyDescription() {
+  const responseRate = await fetch(getExchangeRateLinkAPI());
+  const responseCurrencyDescription = await fetch(
+    "https://api.exchangerate.host/symbols"
+  );
+
+  rateData = await responseRate.json();
+  currencyDescription = await responseCurrencyDescription.json();
+  calculateCurrency();
+}
+
+let fromCurrencyValue = [];
+let toCurrencyValue = [];
 function getExchangeRateLinkAPI() {
   // Populate the select element from-currency and to-currency with Option elements
-  fromCurrency.append(createOptionElement());
-  toCurrency.append(createOptionElement());
+  fromCurrency.append(createOptionElement("from"));
+  toCurrency.append(createOptionElement("to"));
 
-  const fromCurrencyValue = selectElement("from-currency");
-  const toCurrencyValue = selectElement("to-currency");
+  chooseDefaultCurrency(fromCurrency);
+  chooseDefaultCurrency(toCurrency);
+
+  fromCurrencyValue = selectElement("from-currency");
+  toCurrencyValue = selectElement("to-currency");
 
   const urlConvertAPI = `https://api.exchangerate.host/convert?from=${fromCurrencyValue}&to=${toCurrencyValue}`;
   return urlConvertAPI;
+}
+
+// Function to choose default option from-currency
+function chooseDefaultCurrency(selectElement) {
+  for (let index = 0; index < selectElement.length; index++) {
+    const option = selectElement[index];
+    if (option.value === "SGD" && option.classList.value === "from") {
+      option.setAttribute("selected", true);
+    } else if (option.value === "IDR" && option.classList.value === "to") {
+      option.setAttribute("selected", true);
+    }
+  }
 }
 
 // Function to select element and return its value
@@ -22,8 +60,8 @@ function selectElement(id) {
   return value;
 }
 
-// Function to create Option Element
-function createOptionElement() {
+// Function to create Option Element From Currency
+function createOptionElement(className) {
   // DocumentFragment() is like making an empty element that can be use in DOM structure
   fragment = new DocumentFragment();
 
@@ -31,6 +69,14 @@ function createOptionElement() {
     const optionElement = document.createElement("option");
     optionElement.innerHTML = `${item.country}`;
     optionElement.setAttribute("value", `${item.currency_code}`);
+
+    // Assign class = from and class = to respectively to fromCurrency and toCurrency element.
+    if (className === "from") {
+      optionElement.classList.add("from");
+    } else if (className === "to") {
+      optionElement.classList.add("to");
+    }
+
     fragment.append(optionElement);
   });
 
@@ -46,6 +92,8 @@ async function getCountryInformations() {
     "https://jeffrymahbuubi.github.io/currency-api/data/src/country-by-currency-code.json";
   const response = await fetch(apiUrl);
   apiCountry = await response.json();
+  getExchangeRateLinkAPI();
+  getRateAndCurrencyDescription();
 }
 
 getCountryInformations();
